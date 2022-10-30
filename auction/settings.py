14603574e8b490fff4d26dcb1b8c965e7ffe3a2d
harsh <harsh220902@gmail.com>
2022-10-30 21:56:36 +0530

    zeet setup

--- auction/settings.py
+++ auction/settings.py
@@ -21,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0ke6fr@$0xti6lj257@!#ajq9)^33sz)*!^7sr62yt=sj%bo5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get('DEBUG') == 1
+DEBUG = False
 
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS += [environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
