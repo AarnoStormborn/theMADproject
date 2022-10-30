5fe5266745bf7c76b298999dcb47a2289cc066d2
harsh <harsh220902@gmail.com>
2022-10-30 22:24:26 +0530

    Railway setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn auction.wsgi --log-file -
--- auction/settings.py
+++ auction/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = DEFAULT_APPS + MY_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
